bffce448ac1608e389105540b49392867d1051c3
mkgupta2000 <105657874+mkgupta2000@users.noreply.github.com>
2023-01-03 16:03:05 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <nav>
       <ul>
+        {" "}
         <li>
-          <NavLink className={"index-link"} to="/">
+          <NavLink className="index-link" to="/">
             Index
           </NavLink>
         </li>
         <li>
-          <NavLink className={"home-link"} to="/home">
+          <NavLink className="home-link" to="/home">
             Home
           </NavLink>
         </li>
